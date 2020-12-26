1c16e710bc399a487cb200804f301d07151c8cb8
Scott Tresor <scotttresor@gmail.com>
2020-12-26 12:38:12 +0200

    (feat):add studentSubscriptionRequest

--- app/Http/Controllers/Api/Requests/StudentSubscriptionRequest.php
+++ app/Http/Controllers/Api/Requests/StudentSubscriptionRequest.php
@@ -2,11 +2,23 @@
 
 
 namespace App\Http\Controllers\Api\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+
 /**
  * Class StudentSubscriptionRequest
  * @package App\Http\Controllers\Api\Requests
  */
-class StudentSubscriptionRequest
+class StudentSubscriptionRequest extends  FormRequest
 {
+    public function authorize(): bool
+    {
+        return true;
+    }
 
+    public function rules(): array
+    {
+        return [
+
+        ];
+    }
 }
