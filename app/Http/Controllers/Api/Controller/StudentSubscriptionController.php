1f67290174a02c1bd04c43c97e95d49d6860f4b8
Scott Tresor <scotttresor@gmail.com>
2020-12-26 13:51:44 +0200

    (feat):add symfony/form in laravel project

--- app/Http/Controllers/Api/Controller/StudentSubscriptionController.php
+++ app/Http/Controllers/Api/Controller/StudentSubscriptionController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Api\Controller;
+use App\Models\StudentResult;
+use Illuminate\Http\Request;
+
 /**
  * Class StudentSubscriptionController
  * @package App\Http\Controllers\Api\Controller
@@ -8,4 +11,8 @@ namespace App\Http\Controllers\Api\Controller;
 class StudentSubscriptionController
 {
 
+    public function store(Request $request)
+    {
+        $student = new StudentResult();
+    }
 }
